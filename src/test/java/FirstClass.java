fa384ff4164d687654b02331b20707953ac28538
F-Derya <fdtatar@gmail.com>
2021-12-24 23:06:37 +0100

    Second Line

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -4,6 +4,6 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("First line");
 
-        //System.out.println("Second Line");
+        System.out.println("Second Line");
     }
 }
